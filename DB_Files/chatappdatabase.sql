c6b10febc3a5024b09f5e662039b0a6920779458
shanetorres <stor314@gmail.com>
2018-02-20 15:08:13 -0500

    Updated Database Script
    -Weak Entities are functioning properly
    -Merged Logins data into User and added new login attributes
    -Created UserStatus table

--- DB_Files/chatappdatabase.sql
+++ DB_Files/chatappdatabase.sql
@@ -3,19 +3,30 @@ create database ChatApp;
 use ChatApp;
 
 create table Users
-(UserID varchar(20) not null,
+(UserID int not null,
 CreateDate date,
 Active bool,
+FirstName varchar(20),
+LastName varchar(20),
+Username varchar(20),
+Email varchar(30),
+Password varchar(20),
 constraint Pk_UserID primary key (UserID));
 
 create table Logins
 (
-UserID varchar(20) not null primary key references Users (UserID)
-on delete cascade on update cascade,
-FirstName varchar(20),
-LastName varchar(20),
-email varchar(30),
-UserPassword varchar (20));
+UserID int,
+FailedAttempts int,
+SuccessfulAttempts int,
+constraint fk_LoginID foreign key (UserID) references Users (UserID),
+constraint pk_LoginID primary key (UserID));
+
+create table UserStatus
+(
+UserID int,
+UserStatus varchar(20),
+constraint fk_status foreign key (UserID) references Logins (UserID)
+);
 
 create table Groups
 (GroupID int not null,
@@ -25,37 +36,39 @@ Active bool,
 constraint Pk_GroupID primary key (GroupID));
 
 create table UserGroups
-(GroupID int not null references Groups (GroupID)
-on delete cascade on update cascade,
-UserID varchar(20) not null references Users (UserID)
-on delete cascade on update cascade,
+(GroupID int,
+UserID int,
+constraint fk_GroupID foreign key (GroupID) references Groups (GroupID),
+constraint fk_UserID foreign key (UserID) references Users (UserID),
 constraint PK_UserNGroups primary key (GroupID, UserID)
 );
 
 create table Messages
 (
 MessageID int not null,
-UserID varchar(20) references Users (UserID)
-on delete cascade on update cascade,
-RecipientID varchar(20) references Users (UserID)
-on delete cascade on update cascade,
-constraint PK_Messages primary key (MessageID, UserID, RecipientID),
+UserID int,
+RecipientID int,
+constraint fk_messagesender foreign key (UserID) references Users (UserID),
+constraint fk_messagerecipient foreign key (RecipientID) references Users (UserID),
+constraint PK_Messages primary key (MessageID),
 ParentMessageID int,
 TextBody varchar(500),
 DeliverTime timestamp,
-DeliverDate date);
+);
 
 create table MessageRecipients
 (
-MessageID int references Messages (MessageID)
-on delete cascade on update cascade,
-SenderID varchar(20) references Users (UserID)
-on delete cascade on update cascade,
-RecipientID varchar(20) references Users (UserID)
-on delete cascade on update cascade,
+MessageID int,
+SenderID int,
+RecipientID int,
 GroupID int references Groups (GroupID)
 on delete cascade on update cascade,
-constraint PK_MessageRecipients primary key (MessageID, SenderID, RecipientID, GroupID));
+constraint fk_messageID foreign key (MessageID) references Messages (MessageID),
+constraint fk_senderID foreign key (SenderID) references Messages (UserID),
+constraint fk_recipientID foreign key (RecipientID) references Messages (RecipientID),
+constraint PK_MessageRecipients primary key (MessageID, SenderID, RecipientID, GroupID)
+
+);
 
 create table Photos
 (
@@ -63,15 +76,5 @@ PhotoID int not null,
 constraint PK_PhotoID primary key (PhotoID),
 MessageID int,
 FileName varchar(20),
-constraint FK_MessageID foreign key (MessageID) references Messages (MessageID)
-on delete cascade on update cascade);
-
-
-
-
-
-
-
-
-
-
+constraint FK_PhotoMessageID foreign key (MessageID) references Messages (MessageID)
+);
