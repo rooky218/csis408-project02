986c89ffa4b5ce21f7989964bb89f156c34ab658
shanetorres <stor314@gmail.com>
2018-03-01 18:29:26 -0500

    Created Sample Queries for the pages

---
+++ DB_Files/sampledata.sql
@@ -0,0 +1,4 @@
+insert Users select true, 1, '2018/03/01', true, '111-222-3333', '2222', 'Shane', 'Torres', 'storres', 'storres@gmail.com', '24515', 'password1', 'Lynchburg', '1998/06/09';
+insert Users select true, 2, '2018/03/01', true, '112-223-3334', '2322', 'Bob', 'Smith', 'bsmith', 'bsmith@gmail.com', '24515', 'password2', 'Lynchburg', '1996/07/01';
+
+
