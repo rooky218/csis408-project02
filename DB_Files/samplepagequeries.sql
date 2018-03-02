3b3b6d94a32df8ae38b09cb588e47c841e311a22
Shane Torres <stor314@gmail.com>
2018-03-02 11:40:54 -0500

    quick edit

--- DB_Files/samplepagequeries.sql
+++ DB_Files/samplepagequeries.sql
@@ -7,7 +7,7 @@ update Users set FirstName = 'new value' where UserID = somevariable;
 update Users set LastName = 'new value' where UserID = somevariable;
 update Users set Username = 'new value' where UserID = somevariable;
 update Users set Email = 'new value' where UserID = somevariable;
-update Users set FirstName = '0000/00/00' where UserID = somevariable;
+update Users set BirthDate = '0000/00/00' where UserID = somevariable;
 
 -- registration
 insert into Users (Username, UserPassword, Email, Phone) values (variables);
